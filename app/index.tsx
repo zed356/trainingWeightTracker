32f261319000f8e59fa2ab822cb1b7a4301b5728
Z <zilvistam@gmail.com>
2024-10-12 11:09:19 +0100

    added exercise lists

--- app/index.tsx
+++ app/index.tsx
@@ -8,12 +8,22 @@ export default function HomeScreen() {
       flex: 1,
       justifyContent: "center",
       alignItems: "center",
+      marginTop: 30,
+      marginBottom: 30,
     },
   });
 
+  const workoutA = [{ name: "Squat" }, { name: "Flat Bench Press" }, { name: "Barbell Rows" }];
+  const workoutB = [
+    { name: "Squat" },
+    { name: "Overhead Press" },
+    { name: "Deadlift", increase: 5 },
+  ];
+
   return (
     <View style={styles.container}>
-      <ExerciseList />
+      <ExerciseList name={"Day A"} options={workoutA} />
+      <ExerciseList name={"Day B"} options={workoutB} />
     </View>
   );
 }
